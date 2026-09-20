534e6678e8e99d6a1e160a10dd3b076b75a15de1
sanchezibnalehandro-dev <SANCHEZIBNALEHANDRO@GMAIL.COM>
2026-09-20 21:08:52 +0300

    feat: add TASIGO ESG brand preset

---
+++ brand-presets/tasigo-esg.js
@@ -0,0 +1,10 @@
+export const TASIGO_ESG_BRAND = {
+  key: 'tasigo-esg',
+  name: 'Маршрут устойчивого развития',
+  mark: 'М',
+  productName: 'Форум ESG',
+  csvPrefix: 'tasigo-esg-qna',
+  logoUrl: null,
+  faviconUrl: null,
+  coBrand: 'TASIGO × РБК Татарстан'
+};
